3f01578a2d85f9d92bcb553b2f3f7af34fffd144
Andy Pak <147354924+PakmanGames@users.noreply.github.com>
2024-05-14 13:59:34 -0400

    Make changes to 03 solution and comments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -10,7 +10,15 @@ const reverseString = function(string) {
     return reverseString;
 };
 
-reverseString("hello");
+/*
+This method also works
+.split("") splits the string by each individual character turning it into an array
+.reverse() is an array method that reverses an array
+.join("") does the opposite of .split("") it joins all the characters into a string
+*/
+// const reverseString = function (string) {
+//     return string.split("").reverse().join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
