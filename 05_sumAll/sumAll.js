d20ab70f0ada21bd2e3c68c6a62d8028f3c5c662
Andy Pak <147354924+PakmanGames@users.noreply.github.com>
2024-05-14 16:23:52 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof(a) !== "number" || typeof(b) !== "number") {
+        return "ERROR";
+    } else if (a > b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    let sum = 0;
+    for (let i = a; i < b + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
