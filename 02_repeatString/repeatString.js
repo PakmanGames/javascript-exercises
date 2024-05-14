f92e8a0a2258d404607f4c8a8d6df720e228e007
Andy Pak <147354924+PakmanGames@users.noreply.github.com>
2024-05-14 13:55:45 -0400

    Complete exercises 01 to 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    catString = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = num; i >= 1; i--) {
+        catString += string;
+    }
+    return catString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverseArr = [];
+    let reverseString = '';
+    for (let i = 0; i < string.length; i++) {
+        reverseArr[i] = string.charAt(i);
+    }
+    for (let i = reverseArr.length - 1; i >= 0; i--) {
+        reverseString += reverseArr[i];
+    }
+    return reverseString;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
