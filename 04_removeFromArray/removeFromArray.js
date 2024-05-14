33afbafd215d42ff62afbf71e315eb13ef31c317
Andy Pak <147354924+PakmanGames@users.noreply.github.com>
2024-05-14 16:09:47 -0400

    Complete 04 exercise, includes comments for comprehension

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    // Use .filter() method, if True keep element, if false dont' keep
+    // inside .filter() is another function
+    return arr.filter(
+        // Arrow function that has parameter element (element of arr)
+        // .includes() method checks if args contains element
+        // if not it returns false but !false = true
+        // Thus that true is returned to the .filter() method
+        (element) => !args.includes(element)
+    );
 };
 
 // Do not edit below this line
